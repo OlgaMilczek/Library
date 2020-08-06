93286541ecba4a2094cb7815b8c8672ef57b18c5
Olga Nowakowska <o.w.nowakowska@gmail.com>
2020-08-06 15:31:06 +0200

    All bugs fixed.

--- script.js
+++ script.js
@@ -8,34 +8,38 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    const newBookTitle = document.querySelector('input[name=title]');
-    const newBookAuthor = document.querySelector('input[name=author]');
-    const newBookPages = document.querySelector('input[name="pages"]');
-    const newBookRead = document.querySelector('input[name=read]:checked');
-    console.log(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
-    /* let newBook = new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.value);
-    console.log(newBook)
+    const newBookTitle = document.querySelector('input[name=title]').value;
+    const newBookAuthor = document.querySelector('input[name=author]').value;
+    const newBookPages = document.querySelector('input[name="pages"]').value;
+    const newBookRead = document.querySelector('input[name=read]:checked').value;
+    let newBook = new Book(newBookTitle, newBookAuthor, newBookPages, newBookRead);
     myLibrary.push(newBook);
     render();
-    return (newBook); */
+    return (newBook);
   }
 
-  let bookContainer = document.querySelector('#conteiner');
+  const bookContainer = document.querySelector('#book-conteiner');
 
 
 function render() {
+    bookContainer.innerHTML = "";
     myLibrary.forEach(book => {
         const bookLine = document.createElement('div');
         bookLine.classList.add('book-position');
         for (let prop in book) {
             const bookProp = document.createElement('div');
             if(prop === 'read') {
-                if (book[prop]=== 'true'){
-                    bookProp.textContent = "Yes";
+                const yesNoButton = document.createElement('button');
+                if (book[prop]==='true') {
+                    yesNoButton.textContent = "Yes";
+                    yesNoButton.classList.add('yesButton');
                 }
                 else {
-                    bookProp.textContent = "No";
+                    yesNoButton.textContent = "No";
+                    yesNoButton.classList.add('noButton');
                 }
+                yesNoButton.addEventListener('click', readChange);
+                bookProp.appendChild(yesNoButton);
             }
             else {
                 bookProp.textContent = book[prop];
@@ -44,6 +48,7 @@ function render() {
         }
         const dellButton = document.createElement('button');
         dellButton.classList.add('dellButton');
+        dellButton.addEventListener('click', deleteBook)
         bookLine.appendChild(dellButton);
         bookContainer.appendChild(bookLine);
     });
@@ -59,6 +64,7 @@ newBookButton.addEventListener('click', openForm)
 newBookAdded.addEventListener('click', () => {
     addBookToLibrary();
     closeForm();
+    cleanForm();
 });
 
 
@@ -70,4 +76,29 @@ function openForm() {
 function closeForm() {
     overlay.classList.remove('active');
     form.classList.remove('active');
+}
+
+function cleanForm() {
+    document.querySelector('input[name=title]').value = '';
+    document.querySelector('input[name=author]').value = '';
+    document.querySelector('input[name="pages"]').value = '';
+    const newBookRead = document.querySelector('input[name=read]:checked');
+    newBookRead.checked = false;
+}
+
+function readChange() {;
+    if (this.textContent === 'No') {
+        this.textContent = 'Yes';
+    }
+    else {
+        this.textContent = 'No';
+    }
+    this.classList.toggle('yesButton');
+    this.classList.toggle('noButton');
+}
+
+function deleteBook() {
+    const deleteBookTitle = this.parentElement.firstChild.textContent;
+    myLibrary = myLibrary.filter(book => book.title !== deleteBookTitle);
+    render();
 }
